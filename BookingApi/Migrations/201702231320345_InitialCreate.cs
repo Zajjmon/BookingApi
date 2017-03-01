cc0c1c783e64f279d9bcc2f953d5ebdcc45ffe7a
Zajjmon <denzajjmon@outlook.com>
2017-03-01 13:29:13 +0100

    Add project files.

---
+++ BookingApi/Migrations/201702231320345_InitialCreate.cs
@@ -0,0 +1,44 @@
+namespace BookingApi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Bookings",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Description = c.String(),
+                        StartTime = c.DateTime(nullable: false),
+                        EndTime = c.DateTime(nullable: false),
+                        UserId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Password = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Bookings", "UserId", "dbo.Users");
+            DropIndex("dbo.Bookings", new[] { "UserId" });
+            DropTable("dbo.Users");
+            DropTable("dbo.Bookings");
+        }
+    }
+}
---
+++ BookingApi/Models/Booking.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BookingApi.Models
+{
+    public class Booking
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:r}")]
+        public DateTime StartTime { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime EndTime { get; set; }
+
+        public User User { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+    }
+}
